062b0d043cce2f2108a68a3ec640d5705d1526eb
sherr2018 <sherinv017@gmail.com>
2018-11-10 18:54:02 +0530

    wp_install_v002

--- wp_install.sh
+++ wp_install.sh
@@ -1,7 +1,22 @@
+#!/bin/bash
+# GET ALL USER INPUT
 echo "Domain Name (eg. example.com)?"
 read DOMAIN
+echo "Username (eg. opencart)?"
+read USERNAME
 
+echo "Updating OS................."
+sudo apt-get update
+sudo apt install pwgen -y
 
+echo "Sit back and relax :) ......"
+cd /etc/nginx/sites-available/
+wget -O "$DOMAIN" https://goo.gl/s8pdtv
+sed -i -e "s/example.com/$DOMAIN/" "$DOMAIN"
+sed -i -e "s/www.example.com/www.$DOMAIN/" "$DOMAIN"
+sudo ln -s /etc/nginx/sites-available/"$DOMAIN" /etc/nginx/sites-enabled/
+echo "Setting up Wordpress With Cloudflare FULL SSL"
+sleep 2;
 sudo mkdir /var/www/"$DOMAIN"
 cd /var/www/"$DOMAIN"
 sudo su -c 'echo "<?php phpinfo(); ?>" |tee info.php'
@@ -16,3 +31,17 @@ sleep 2;
 cd ~
 sudo chown www-data:www-data -R /var/www/"$DOMAIN"
 sudo systemctl restart nginx.service
+
+PASS=`pwgen -s 14 1`
+
+mysql -uroot <<MYSQL_SCRIPT
+CREATE DATABASE $USERNAME;
+CREATE USER '$USERNAME'@'localhost' IDENTIFIED BY '$PASS';
+GRANT ALL PRIVILEGES ON $USERNAME.* TO '$USERNAME'@'localhost';
+FLUSH PRIVILEGES;
+MYSQL_SCRIPT
+
+echo "Here is the database"
+echo "Database:   $USERNAME"
+echo "Username:   $USERNAME"
+echo "Password:   $PASS"
